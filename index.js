aaf60d7e4e3018fd5a1650f3a76304831a7b5043
Bharatrathod-81 <Bharat21@navgurukul.org>
2021-12-06 20:03:12 +0530

    debugging

--- index.js
+++ index.js
@@ -1,39 +1,45 @@
-const billAmount = document.querySelector("#bill-amount");
-const cashGiven = document.querySelector("#cash-given");
-const checkButton = document.querySelector("#check-button");
-const message = document.querySelector("#error-message");
-const noOfNotes = document.querySelectorAll(".no-of-notes");
+var billAmount = document.querySelector("#bill-amount");
+var cashGiven = document.querySelector("#cash-given");
+var checkButton = document.querySelector("#check-button");
+var message = document.querySelector("#error-message");
+var noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1]; 
-var given = cashGiven.value;
-var bill = billAmount.value;
 
 checkButton.addEventListener("click",function validateAmount() {
+    var given = Number(cashGiven.value);
+    var bill = Number(billAmount.value);
     message.style.display = "none";
-    if (billAmount.value > 0) {
-        if ((given >= bill)){
-            const amountToBeReturned = cashGiven.value - billAmount.value;
+    if (bill>0) {
+        if (given >= bill) {
+            var amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
 
-        }else{
-            errorHandler("The cash provided should atleast be equal to the bill amount");
+        } 
+        else {
+            
+            errorHandler(
+                'The cash provided should atleast be equal to the bill amount'
+            )
         }
-    }else {
-         errorHandler("Invalid bill amount");
-    };
+    } 
+    else {
+        
+        errorHandler(
+            'Invalid Value'
+        )    };
 });
 
-function calculateChange(amountToBeReturned){
-    for(let i=0; i<availableNotes.length; i++){
-        const numberOfNotes = Math.trunc(
+function calculateChange(amountToBeReturned) {
+    for(let i=0; i<availableNotes.length; i++) {
+        var numberOfNotes = Math.trunc(
             amountToBeReturned/availableNotes[i]
         );
-        if (numberOfNotes >= 0) {
-            amountToBeReturned=amountToBeReturned%availableNotes[i];
+        amountToBeReturned=amountToBeReturned%availableNotes[i];
+        
+        if (numberOfNotes > 0)  {
             noOfNotes[i].innerText= numberOfNotes;
-        }else{
-            errorHandler("The cash provided should atleast be equal to the bill amount");
-        }
+        } 
     };
 };
 
